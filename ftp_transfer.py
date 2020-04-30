b638505c27a90c226afaf7843bf98457ae18fac7
Tomáš Roj <rojtomas@email.cz>
2020-04-30 11:20:36 +0200

    Added support for the file name 😎

--- ftp_transfer.py
+++ ftp_transfer.py
@@ -15,13 +15,15 @@ print("\n An easy way to tranfer all your files into remote FTP server. \n Quick
 @click.option("--file", help="Set path to file you want to transfer")
 
 def transfer(server, username, password, file):
-    file_extension = os.path.splitext(file)[1]
+    file_name = os.path.basename(file)
+    (file_name, ext) = os.path.splitext(file_name)
 
     session = ftplib.FTP(server, username, password)
+    print("Transfering files...")
     file = open(file,'rb')
-    session.storbinary('STOR transferedFile' + str(file_extension), file)  
+    session.storbinary('STOR ' + str(file_name) + str(ext), file)  
     file.close()
-    print(" File transferes succesfully! Quiting session...")
+    print(" File transferes succesfully!")
     session.quit()
     print(" Session terminated.")
 
